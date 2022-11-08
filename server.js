f5d4e56949bc63944ba5e00f0e94a67c297568fd
Josh <zylo.codes@gmail.com>
2022-11-07 21:35:37 -0600

    Persist data

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
+const fs = require('fs')
 const path = require('path')
 const express = require('express')
 const app = express()
 const PORT = 3000
-const animalData = require('./animals.json')
 
 app.use(express.static("public"))
 app.use(express.json())
 
 // API routes
 app.get('/api/animals', (req, res) => {
-  res.json(animalData)
+  // read animals.json contents
+  fs.readFile(path.join(__dirname, "animals.json"), 'utf-8', function(err, data) {
+    if (err) {
+      res.status(500).json(err)
+      return
+    }
+    const json = JSON.parse(data)
+    // respond with the parsed array
+    res.json(json)
+  }) 
 })
 
 app.post('/api/animals', (req, res) => {
@@ -25,9 +34,25 @@ app.post('/api/animals', (req, res) => {
     id: Math.random()
   }
 
-  animalData.push(newAnimal)
-
-  res.json(newAnimal)
+  // Read contents of animals.json
+  fs.readFile(path.join(__dirname, "animals.json"), "utf-8", function(err, data) {
+    if (err) {
+      res.status(500).json(err)
+      return
+    }
+    // parse string into JSON
+    const animalData = JSON.parse(data)
+    // push our animal into json
+    animalData.push(newAnimal)
+    // stringify animals array and save file
+    fs.writeFile(path.join(__dirname, "animals.json"), JSON.stringify(animalData), function(err) {
+      if (err) {
+        res.status(500).json(err)
+        return
+      }
+      res.status(200).json(newAnimal)
+    })
+  })
 })
 
 app.get('/api/animals/:animalType', (req, res) => {
@@ -40,13 +65,20 @@ app.get('/api/animals/:animalType', (req, res) => {
     return
   }
   
-  const results = animalData.filter(animal => animal.type === animalType)
+  fs.readFile(path.join(__dirname, 'animals.json'), 'utf-8', function(err, data) {
+    if (err) {
+      res.status(500).json(err)
+      return
+    }
+    const animalData = JSON.parse(data)
+    const results = animalData.filter(animal => animal.type === animalType)
 
-  if (results.length === 0) {
-    res.status(404)
-  }
+    if (results.length === 0) {
+      res.status(404)
+    }
 
-  res.json(results)
+    res.json(results)
+  })
 })
 
 
