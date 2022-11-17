dcbbece3336cf2bc47e22e4d17b4a23c202988bf
Josh <zylo.codes@gmail.com>
2022-11-16 20:05:51 -0600

    Delete route!

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,17 @@
 const router = require('express').Router()
 const path = require('path')
 const fs = require('fs')
+const connection = require('../db/connection')
 
 const dbPath = path.join(__dirname, "..", "db", "animals.json")
 
-router.get('/animals', (req, res) => {
-  // read animals.json contents
-  fs.readFile(dbPath, 'utf-8', function(err, data) {
-    if (err) {
-      res.status(500).json(err)
-      return
-    }
-    const json = JSON.parse(data)
-    // respond with the parsed array
-    res.json(json)
-  }) 
+router.get('/animals', async (req, res) => {
+  try {
+    const [results] = await connection.promise().query("SELECT * FROM animals")
+    res.status(200).json(results)
+  } catch(err) {
+    res.status(500).json(err)
+  }
 })
 
 router.post('/animals', (req, res) => {
@@ -77,29 +74,19 @@ router.get('/animals/:animalType', (req, res) => {
   })
 })
 
-router.delete('/animals/:id', (req, res) => {
+router.delete('/animals/:id', async (req, res) => {
   const id = req.params.id
 
   if (!id) {
     return res.status(400).json({ error: "We need an id" })
   }
 
-  // read json file
-  fs.readFile(dbPath, "utf8", function(err, data) {
-    // parse the contents
-    const animalData = JSON.parse(data)
-    // modify contents
-    const updatedAnimalData = animalData.filter(animal => id != animal.id)
-    // stringify contents and re-save file
-    fs.writeFile(dbPath, JSON.stringify(updatedAnimalData), function(err) {
-      if (err) {
-        return res.status(500).json(err)
-      }
-      res.json(true)
-    })
-  })
-
-  console.log('Delete route hit!')
+  try {
+    await connection.promise().query("DELETE FROM animals WHERE id = ?", [id])
+    res.json(true)
+  } catch(err) {
+    res.status(500).json(err)
+  }
 })
 
 module.exports = router
